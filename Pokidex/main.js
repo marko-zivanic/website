62d82476388efdd7fa295b9071c14c22b8586037
marko-zivanic <151414136+marko-zivanic@users.noreply.github.com>
2023-11-20 20:07:35 +0100

    Update main.js

--- Pokidex/main.js
+++ Pokidex/main.js
@@ -15,7 +15,7 @@ async function fetchData() {
     try {
         const response = await $.ajax({
             method: 'GET',
-            url: 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0'
+            url: 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'
         });
 
         array = response.results;
